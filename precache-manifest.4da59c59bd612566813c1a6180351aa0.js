8d911e7b3ced12c8d3a38274ae71a2d6cefa13e1
nikolinareic03 <nikolina.reic@gmail.com>
2020-05-14 11:13:41 +0200

    Updates

---
+++ precache-manifest.4da59c59bd612566813c1a6180351aa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30cdb2c6148b6f5c2ed187d45a4878dc",
+    "url": "/react-proba2/index.html"
+  },
+  {
+    "revision": "26c9e9875a870f9619db",
+    "url": "/react-proba2/static/css/main.12a05e8f.chunk.css"
+  },
+  {
+    "revision": "9977e4478887f32da208",
+    "url": "/react-proba2/static/js/2.630ec583.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-proba2/static/js/2.630ec583.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26c9e9875a870f9619db",
+    "url": "/react-proba2/static/js/main.ef033987.chunk.js"
+  },
+  {
+    "revision": "fc45ddb9760b2331fccb",
+    "url": "/react-proba2/static/js/runtime-main.bdcb178f.js"
+  },
+  {
+    "revision": "78f6d1008890c7d6662842e3478b98e1",
+    "url": "/react-proba2/static/media/split.78f6d100.jpg"
+  }
+]);
